1f2d74d938a6838b92226e0264f5251097fa371d
ltetur <166016726+ltetur@users.noreply.github.com>
2024-05-12 11:23:47 +0200

    Update Acronym.java
    Testik

--- exercises/practice/acronym/src/main/java/Acronym.java
+++ exercises/practice/acronym/src/main/java/Acronym.java
@@ -1,9 +1,9 @@
 class Acronym {
 
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        throw new UnsupportedOperationException("Delete this statement and write your fdhdfnnc own ffimplementation.");
     }
-
+fd
     String get() {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
